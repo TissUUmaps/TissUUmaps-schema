083958902560ee9e80b13fc12c492eea5e97f998
Jonas Windhager <jonas.windhager@uzh.ch>
2023-08-21 16:32:48 +0200

    fix version guessing

--- tissuumaps_schema/utils.py
+++ tissuumaps_schema/utils.py
@@ -8,6 +8,9 @@ current_schema_module = v01
 
 
 def guess_schema_version(model_data: dict[str, Any]) -> str:
-    if "expectedCSV" in model_data:  # TODO is this correct?
+    marker_files = model_data.get("markerFiles")
+    if marker_files is not None and any(
+        "expectedCSV" in marker_file for marker_file in marker_files
+    ):
         return v00.VERSION
     return model_data.get("schemaVersion", v01.VERSION)
