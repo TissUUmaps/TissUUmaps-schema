52e886bfbbbcd7f9b47ee73a2a9d3f6f1b30fb50
Jonas Windhager <jonas.windhager@scilifelab.se>
2023-10-05 16:41:42 +0200

    undo upgrade on parse

--- tissuumaps_schema/base.py
+++ tissuumaps_schema/base.py
@@ -2,12 +2,6 @@ from typing import Any, ClassVar, Optional, Type, TypeVar
 
 from pydantic import BaseModel, ConfigDict, Field
 
-from tissuumaps_schema.utils import (
-    MAJOR_SCHEMA_VERSION_MODULES,
-    get_major_version,
-    guess_schema_version,
-)
-
 
 class SchemaBaseModel(BaseModel):
     model_config = ConfigDict(extra="forbid")
@@ -24,11 +18,7 @@ class RootSchemaBaseModel(SchemaBaseModel):
     def parse(
         cls: Type[TRoot], model_data: dict[str, Any], strict: Optional[bool] = None
     ) -> TRoot:
-        schema_version = guess_schema_version(model_data)
-        major_schema_version = get_major_version(schema_version)
-        schema_module = MAJOR_SCHEMA_VERSION_MODULES[major_schema_version]
-        model_instance = schema_module.model_validate(model_data, strict=strict)
-        return cls.upgrade(model_instance)
+        return cls.model_validate(model_data, strict=strict)
 
     @classmethod
     def upgrade(cls: Type[TRoot], old_model_instance: "RootSchemaBaseModel") -> TRoot:
