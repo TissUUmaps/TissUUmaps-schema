c00b2df2a39a9665e9dcea62605403fc557c26bc
Christophe Avenel <christophe.avenel@it.uu.se>
2023-10-11 10:46:05 +0200

    Put back _no_outline alias to not break compatibility between minor version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,7 +14,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 ### Changed
-- Renamed field `_no_outline` to `no_outline` (in JSON)
 
 ### Removed
 
--- tissuumaps_schema/v01/project.py
+++ tissuumaps_schema/v01/project.py
@@ -349,6 +349,7 @@ class ExpectedRadios(SchemaBaseModel):
     )
     no_outline: bool = Field(
         default=False,
+        alias="_no_outline",
         description="If marker shapes should be rendered without outline.",
     )
     no_fill: bool = Field(
